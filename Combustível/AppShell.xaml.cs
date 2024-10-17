3d597f392019458c867123d60b4e3bf1eff586e5
gabri <gabri@DESKTOP-1KKM2F9>
2024-10-17 16:23:29 -0300

    Adicionar arquivos de projeto.

---
+++ Combustível/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Combustível
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Combustível/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Combustível
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Calcular(object sender, EventArgs e)
+        {
+            double Alcool = Convert.ToDouble(Etanol_TXT.Text);
+            double Gasolina = Convert.ToDouble(Gasolina_TXT.Text);
+            string Msg = "";
+
+            if (Alcool <= (Gasolina * 0.7)) {
+
+                Msg = " O Etanol está compensando";
+
+
+            }
+            else {
+                Msg = "A Gasolina está compensando";
+            
+            }
+
+            DisplayAlert  ("Resultado", Msg, "Ok");
+
+        }
+    }
+
+}
